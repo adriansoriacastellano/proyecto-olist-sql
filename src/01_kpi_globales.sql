c87d8346a65a8147a9d627599e8fc11d7ee2bda8
adriansoriacastellano <adriansoriacastellano@gmail.com>
2026-02-03 19:22:12 +0100

    Adicción de explicaciones

--- src/01_kpi_globales.sql
+++ src/01_kpi_globales.sql
@@ -1,4 +1,5 @@
--- ¿Cuánto ha vendido Olist en total en toda su historia según este dataset?
+-- ¿Cuánto ha vendido Olist en total en toda su historia según el dataset?
+
 SELECT 
     SUM(price) AS ventas_totales_historicas,
     COUNT(*) AS numero_total_productos_vendidos,
--- src/02_top_categorias.sql
+++ src/02_top_categorias.sql
@@ -1,13 +1,17 @@
 -- Análisis de las 10 categorías de productos con mayores ingresos totales
+
 SELECT 
+    -- Se utiliza la traducción al inglés para mayor claridad
     t.product_category_name_english AS categoria,
     COUNT(*) AS total_ventas,
     SUM(i.price) AS ingresos_totales
+    -- Se unen las tablas necesarias para obtener la información requerida
 FROM olist_order_items_dataset i
 JOIN olist_products_dataset p 
     ON i.product_id = p.product_id
 JOIN product_category_name_translation t 
     ON p.product_category_name = t.product_category_name
+    -- Se agrupan los resultados por categoría de producto y se ordenan por ingresos totales para obtener las top 10
 GROUP BY 
     t.product_category_name_english
 ORDER BY 
